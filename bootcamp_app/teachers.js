b3211b011aaac431eabedce2f3a6747e105e32f8
Lee Benzacar <LMBenzacar@gmail.com>
2022-02-12 17:35:18 +0000

    added sql protection

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -1,12 +1,15 @@
-const { Pool } = require('pg');
+
+const { Pool } = require("pg");
 
 const pool = new Pool({
-  user: 'vagrant',
-  password: '123',
-  host: 'localhost',
-  database: 'bootcampx'
+  user: "vagrant",
+  password: "123",
+  host: "localhost",
+  database: "bootcampx"
 });
 
+const cohortName = process.argv[2];
+const values = [`%${cohortName}%`];
 
 pool.query(`
 SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
@@ -14,11 +17,12 @@ FROM teachers
 JOIN assistance_requests ON teacher_id = teachers.id
 JOIN students ON student_id = students.id
 JOIN cohorts ON cohort_id = cohorts.id
-WHERE cohorts.name = '${process.argv[2] || 'JUL02'}'
+WHERE cohorts.name LIKE $1
 ORDER BY teacher;
 `)
 .then(res => {
   res.rows.forEach(row => {
     console.log(`${row.cohort}: ${row.teacher}`);
   })
-});
+})
+.catch(err => console.error("query error", err.stack));
